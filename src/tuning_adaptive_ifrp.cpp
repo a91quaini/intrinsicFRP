2388c9fedc20e5c37805b5494211f4e2727d7032
a91quaini <alberto91quaini@gmail.com>
2023-06-21 09:36:52 +0200

    tuning identification ifrp

--- src/tuning_adaptive_ifrp.cpp
+++ src/tuning_adaptive_ifrp.cpp
@@ -352,7 +352,12 @@ arma::vec IdentificationScoreAdaptiveIFRPCpp(
     // model score = 0. Otherwise, leave the model as mis-identified, i.e.,
     // model score = 1.
     if (output(1) < test_size) {
-      model_score(arma::span(par,  aifrp.n_cols - 1)) = 0;
+      Rcpp::Rcout << "idx_selected = " << idx_selected << "\n";
+      Rcpp::Rcout << "stat = " << output(0) << "\n";
+      Rcpp::Rcout << "p-value = " << output(1) << "\n";
+      model_score(arma::span(par,  aifrp.n_cols - 1)) = arma::zeros(
+        aifrp.n_cols - par
+      );
       break;
     }
 
